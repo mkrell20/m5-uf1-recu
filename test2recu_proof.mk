796b96ee9885f2b8d83d2b6abf3aea8adfdde180
mkrell20 <mkrell20@ilg.cat>
2022-05-24 19:13:33 +0200

    branca_nivell2

---
+++ test2recu_proof.mk
@@ -0,0 +1,27 @@
+commit 1010e7d9d0fdc9fdad9d46623b9f269eec109f4c (HEAD -> nivell2recu)
+Author: mkrell20 <mkrell20@ilg.cat>
+Date:   Tue May 24 19:10:21 2022 +0200
+
+    test2recu KILLED: MK
+
+commit 88a73bac1d0406fd592304c0bfbdb9ffa053c39d (origin/nivell2recu)Author: mkrell20 <mkrell20@ilg.cat>
+Date:   Tue May 24 19:08:45 2022 +0200
+
+:...skipping...
+commit 1010e7d9d0fdc9fdad9d46623b9f269eec109f4c (HEAD -> nivell2recu)
+Author: mkrell20 <mkrell20@ilg.cat>
+Date:   Tue May 24 19:10:21 2022 +0200
+
+    test2recu KILLED: MK
+
+commit 88a73bac1d0406fd592304c0bfbdb9ffa053c39d (origin/nivell2recu)Author: mkrell20 <mkrell20@ilg.cat>
+Date:   Tue May 24 19:08:45 2022 +0200
+
+    MK: commit test2recu
+
+commit aa14ff5f1d12fee162998f08bb54a2bc01db9a83
+Author: mkrell20 <mkrell20@ilg.cat>
+Date:   Tue May 24 19:06:49 2022 +0200
+
+    added MarkDown file
+
